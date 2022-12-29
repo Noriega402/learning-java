3f2e2b5d82e0cc35f7fe5d1087c3efe9b326ac79
Noriega402 <d.noriegachajon@gmail.com>
2022-12-28 19:15:29 -0600

    Mi first commit with java in github

---
+++ src/learning/java/LearningJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package learning.java;
+
+/**
+ *
+ * @author HP INTEL
+ */
+public class LearningJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
