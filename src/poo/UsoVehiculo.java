e3740b22d7531a3faa3f1da9eccd3f754d0fe75a
Noriega402 <d.noriegachajon@gmail.com>
2022-12-28 19:50:04 -0600

    uncomment code of class UsoVehiculo

--- src/poo/UsoVehiculo.java
+++ src/poo/UsoVehiculo.java
@@ -17,7 +17,7 @@ public class UsoVehiculo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*coche miCoche = new coche();
+        coche miCoche = new coche();
         
         miCoche.set_EstablecerColor(JOptionPane.showInputDialog("Escribe el color del Coche"));
         miCoche.set_ConfAsientos(JOptionPane.showInputDialog("Tiene asientos de cuero?"));
@@ -25,9 +25,8 @@ public class UsoVehiculo {
         System.out.println(miCoche.get_DatosGenerales());
         System.out.println(miCoche.get_DimeAsientos());
         System.out.println(miCoche.get_DimePesoCoche());
-        System.out.println("El precio del Coche es de " + miCoche.get_PrecioCoche());*/
+        System.out.println("El precio del Coche es de " + miCoche.get_PrecioCoche());
         
-        coche miCoche = new coche();
         furgoneta miFurgo = new furgoneta(7,2);
         miFurgo.set_EstablecerColor("Azul");
         miFurgo.set_ConfAsientos("No");
